a45ba9dd4fa5bb11ee4d99d112fbf8840c73987f
home26 <45549502+home26@users.noreply.github.com>
2021-01-22 19:29:46 -0500

    Create 239.cpp

---
+++ heap/239.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        priority_queue<pair<int, int>> q;
+        for (int i = 0; i < nums.size(); ++i) {
+            while (!q.empty() && q.top().second <= i - k) q.pop();
+            q.push({nums[i], i});
+            if (i >= k - 1) res.push_back(q.top().first);
+        }
+        return res;
+    }
+};
