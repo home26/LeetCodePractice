486aa7469da4a0da43a90440e21a37683d7a19de
home26 <45549502+home26@users.noreply.github.com>
2021-03-02 21:54:43 -0500

    Create 50Pow.py

---
+++ RecursionBasics/50Pow.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        if n < 0:
+            x = 1 / x
+            n = -n
+        if n % 2:
+            return x * self.myPow(x, n - 1)
+        return self.myPow(x * x, n / 2)    
