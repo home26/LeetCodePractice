9df095ddf621412330c87d51cb3d7446e2c9e3c7
home26 <45549502+home26@users.noreply.github.com>
2021-03-04 21:01:30 -0500

    Create 198HouseRobber.py

---
+++ dynamicPrograming/198HouseRobber.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        N = len(nums)
+        dp = [0] * (N + 1)
+        dp[1] = nums[0]
+        for i in range(1, N):
+            dp[i + 1] = max(dp[i], dp[i - 1] + nums[i])
+        return dp[-1]
